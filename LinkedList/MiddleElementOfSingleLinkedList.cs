c08926bc3174f7c11b24e14f47ea62aa285070db
Ragini340 <ragini31bxr@gmail.com>
2024-11-30 23:47:01 +0530

    #DSA Implementation for Finding Middle Element of Single Linked List

---
+++ LinkedList/MiddleElementOfSingleLinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructure.Linkedlist;
+
+namespace DataStructure.LinkedList
+{
+    public class MiddleElementOfSingleLinkedList
+    {
+        public Node middleElementInLinkedList(Node head)
+        {
+            Node slow = head;
+            Node fast = head;
+
+            while (fast.next != null && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+
+        public static void Main(string[] args)
+        {
+            Node head = new Node(10);
+            head.next = new Node(20);
+            head.next.next = new Node(30);
+            head.next.next.next = new Node(40);
+            head.next.next.next.next = new Node(50);
+            head.next.next.next.next.next = new Node(60);
+
+            MiddleElementOfSingleLinkedList middleElementOfSingleLinkedList = new MiddleElementOfSingleLinkedList();
+            Node isMiddleElementFound = middleElementOfSingleLinkedList.middleElementInLinkedList(head);
+            Console.WriteLine("Middle element is: " + isMiddleElementFound.data);
+        }
+
+    }
+}
