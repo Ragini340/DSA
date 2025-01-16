b690e6a075174cd464e89df2787e63aca2373769
Ragini340 <ragini31bxr@gmail.com>
2025-01-16 19:05:54 +0530

    #DSA  Implementation for finding 4th largest elements from the given array

---
+++ Sorting/KLargestElement.cs
@@ -0,0 +1,65 @@
+﻿namespace DataStructure.Sorting
+{
+    /*
+     You are given an unsorted array containing ‘N’ integers. You need to find ‘K’ largest elements from the given array. 
+     Also, you need to return the elements in non-decreasing order.
+     */
+    public class KLargestElement
+    {
+        public static int FindKthLargestElement(int[] A, int B)
+        {
+            for (int i = 0; i < B; i++)
+            {
+                int min_index = i;
+                int min_element = A[i];
+                for (int j = i; j < A.Length; j++)
+                {
+                    if (A[j] > min_element)
+                    {
+                        min_element = A[j];
+                        min_index = j;
+                    }
+                }
+                int temp = A[i];
+                A[i] = A[min_index];
+                A[min_index] = temp;
+            }
+
+            return A[B - 1];
+        }
+
+        //Sorting using insertion sort 
+        public static int[] InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nThe elements in non-decreasing order are: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            return arr;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, -1, 2, 3, -4 };
+            int result = FindKthLargestElement(arr, 4);
+            Console.WriteLine("Nth largest element is: " + result);
+            InsertionSort(arr);
+        }
+
+    }
+}
