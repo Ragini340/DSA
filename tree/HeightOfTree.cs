a04add045f2a3254688f43ad69d759a0bc34d001
Ragini340 <ragini31bxr@gmail.com>
2025-03-20 10:54:50 +0530

    #DSA Updated Height of Tree Documentation

--- tree/HeightOfTree.cs
+++ tree/HeightOfTree.cs
@@ -2,8 +2,12 @@
 {
     /*
     Time Complexity: O(n), where n is the number of nodes in the binary tree as each node is visited once.
-    Auxiliary Space: O(h), where h is height of the binary tree.*/
+    Space Complexity: O(h), where h is height of the binary tree.*/
     //Output:- Tree height is: 2
+    /*
+     The height of a binary tree is the length of the longest path from the root node to a leaf node. The height of an empty tree is -1,
+     and the height of a tree with only one node is 0.
+    */
     public class HeightOfTree
     {
         public static int TreeHeight(Node root)
