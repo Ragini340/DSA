58883bb00eafb4ec198bfba9e00873ab69875380
Ragini340 <ragini31bxr@gmail.com>
2026-05-18 10:30:00 +0530

    #DSA Program to find two numbers whose sum equals the target.

---
+++ Array/TwoSum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Array
+{
+/*Find two numbers whose sum equals the target.*/
+
+/*Time Complexity (TC): O(n)
+
+Reason:
+- We traverse the array only once.
+- Dictionary lookup (ContainsKey) and insertion are O(1) on average.
+
+Space Complexity (SC): O(n)
+
+Reason:
+- In the worst case, all n elements are stored in the Dictionary.*/
+
+    public class TwoSum
+    {
+        public static int[] FindTwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+
+                map[nums[i]] = i;
+            }
+
+            return new int[] { -1, -1 };
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] result = FindTwoSum(nums, target);
+
+            Console.WriteLine("Indices: " + result[0] + ", " + result[1]);
+        }
+    }
+}
