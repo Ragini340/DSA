f24456f557491da5dc126b6b5bd4fcbad944ea24
Ragini340 <ragini31bxr@gmail.com>
2026-06-22 18:05:41 +0530

    #DSA Updated Selection Sort

--- Sorting/SelectionSort.cs
+++ Sorting/SelectionSort.cs
@@ -43,6 +43,40 @@
             Console.WriteLine("Selection sorting: ");
             SelectionSort.PrintSortedArray(selectionSortArr);
         }
+/*
+Initial Array:
+[1, 5, -1, 2, 3, -4]
 
+Pass 1 (i = 0)
+Minimum Element = -4
+Swap 1 and -4
+[-4, 5, -1, 2, 3, 1]
+
+Pass 2 (i = 1)
+Minimum Element = -1
+Swap 5 and -1
+[-4, -1, 5, 2, 3, 1]
+
+Pass 3 (i = 2)
+Minimum Element = 1
+Swap 5 and 1
+[-4, -1, 1, 2, 3, 5]
+
+Pass 4 (i = 3)
+Minimum Element = 2
+No Swap Needed
+[-4, -1, 1, 2, 3, 5]
+
+Pass 5 (i = 4)
+Minimum Element = 3
+No Swap Needed
+[-4, -1, 1, 2, 3, 5]
+
+Final Sorted Array:
+[-4, -1, 1, 2, 3, 5]
+
+Output:
+-4 -1 1 2 3 5
+*/
     }
 }
