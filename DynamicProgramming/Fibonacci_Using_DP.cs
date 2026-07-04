19d24e3bd4fea9d026dcc98a3a23a5639505068f
Ragini340 <ragini31bxr@gmail.com>
2026-07-04 13:41:39 +0530

    #DSA Updated Fibonacci Using DP

--- DynamicProgramming/Fibonacci_Using_DP.cs
+++ DynamicProgramming/Fibonacci_Using_DP.cs
@@ -45,3 +45,59 @@ namespace DataStructure.DynamicProgramming
         }
     }
 }
+/*
+ Fibonacci(10)
+
+n = 10
+
+Since n > 1:
+a = 0
+b = 1
+
+i = 2
+c = 0 + 1 = 1
+a = 1
+b = 1
+
+i = 3
+c = 1 + 1 = 2
+a = 1
+b = 2
+
+i = 4
+c = 1 + 2 = 3
+a = 2
+b = 3
+
+i = 5
+c = 2 + 3 = 5
+a = 3
+b = 5
+
+i = 6
+c = 3 + 5 = 8
+a = 5
+b = 8
+
+i = 7
+c = 5 + 8 = 13
+a = 8
+b = 13
+
+i = 8
+c = 8 + 13 = 21
+a = 13
+b = 21
+
+i = 9
+c = 13 + 21 = 34
+a = 21
+b = 34
+
+i = 10
+c = 21 + 34 = 55
+a = 34
+b = 55
+
+Return b = 55
+ */
